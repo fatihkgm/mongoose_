5b75ed14ddee48ee501c4c45c2774070c6515a67
bestie06 <fatih.kgokmen@gmail.com>
2020-12-21 21:36:21 -0500

    Data Validation on Mongoose

--- app.js
+++ app.js
@@ -4,38 +4,44 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true});
 
 const fruitSchema = new mongoose.Schema ({
-  name : String,
-  rating: Number,
+  name : {
+    type:String,
+    required:[true,"<<<<<<<<<<<<<Name is required"]
+  },
+  rating:{
+    type:Number,
+    min:1,
+    max:10
+  },
   review:String
 });
 
 const Fruit = mongoose.model("Fruit",fruitSchema);
 
 const fruit= new Fruit({
-  name:"Apple",
   rating: 7,
   review: " Pretty solid. "
 });
 
 
-//person.save();
+fruit.save();
 
-const kivi= new Fruit({
-  name:"KIWI",
-  score:10,
-  revire:"i like it "
-});
-const orange= new Fruit({
-  name:"orange",
-  score:4,
-  revire:"i like it "
-});
+// const kivi= new Fruit({
+//   name:"KIWI",
+//   score:10,
+//   revire:"i like it "
+// });
+// const orange= new Fruit({
+//   name:"orange",
+//   score:4,
+//   revire:"i like it "
+// });
 
-const banana= new Fruit({
-  name:"banana",
-  score:5,
-  revire:"i like it "
-});
+// const banana= new Fruit({
+//   name:"banana",
+//   score:5,
+//   revire:"i like it "
+// });
 
 
 // Fruit.insertMany([kivi,orange,banana], function(err){
